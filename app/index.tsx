debdf5241f359ff68509f3c8294cb3fdeb63acd4
Daniel <deco2449584@gmail.com>
2024-08-01 16:07:35 -0500

    login whit google

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
 import { Redirect } from "expo-router";
-import "dotenv/config";
 export default function Index() {
   return <Redirect href={"/home"} />;
 }
--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,9 +1,29 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import * as WebBrowser from "expo-web-browser";
 import { Colors } from "./../constants/Colors";
 import { StyleSheet } from "react-native";
-
+import { useWarmUpBrowser } from "./../hooks/useWarmUpBrowser";
+import { useOAuth } from "@clerk/clerk-expo";
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
   return (
     <View>
       <View
@@ -56,7 +76,7 @@ export default function LoginScreen() {
           Find your favorite business near your and post your own business to
           your community
         </Text>
-        <TouchableOpacity style={styles.btn}>
+        <TouchableOpacity style={styles.btn} onPress={onPress}>
           <Text
             style={{
               color: "white",
