600c980615e91af3617dc33bfc4f1fbe77273f17
Daniel <deco2449584@gmail.com>
2024-08-13 20:45:00 -0500

    businesslist

--- app/businesslist/[category].js
+++ app/businesslist/[category].js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { collection, getDocs, query, where } from "firebase/firestore";
@@ -9,16 +9,18 @@ export default function BusinessListByCategory() {
   const navigation = useNavigation();
   const { category } = useLocalSearchParams();
   const [businessList, setBusinessList] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     navigation.setOptions({
       headerShown: true,
       headerTitle: category,
     });
-    getBusinessListByCategory();
+    getBusinessList();
   }, []);
 
-  const getBusinessListByCategory = async () => {
+  const getBusinessList = async () => {
     // Fetch business list by category
+    setLoading(true);
     const q = query(
       collection(db, "BusinessList"),
       where("category", "==", category)
@@ -28,16 +30,25 @@ export default function BusinessListByCategory() {
     querySnapshot.forEach((doc) => {
       console.log(doc.data());
       setBusinessList((prev) => [...prev, doc.data()]);
+      setLoading(false);
     });
   };
   return (
     <View>
-      {businessList?.length > 0 ? (
+      {businessList?.length > 0 && loading == false ? (
         <FlatList
           data={businessList}
+          onRefresh={getBusinessList}
+          refreshing={loading}
           renderItem={({ item, index }) => (
             <BusinessListCard business={item} key={index} />
           )}
+        />
+      ) : loading ? (
+        <ActivityIndicator
+          style={{ marginTop: "60%" }}
+          size="large"
+          color={Colors.PRIMARY}
         />
       ) : (
         <Text
@@ -47,7 +58,6 @@ export default function BusinessListByCategory() {
             fontSize: 20,
             fontFamily: "outfit-bold",
             color: Colors.GRAY,
-            textAling: "center",
             margin: 50,
           }}
         >
