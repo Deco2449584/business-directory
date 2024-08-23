eb3133865116b876ee28f77fd7a22c372b6ec2ae
Daniel <deco2449584@gmail.com>
2024-08-23 10:26:54 -0500

    busqueda home

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,73 +1,13 @@
-import { View, Text, Image, FlatList } from "react-native";
-import React, { useState, useEffect } from "react";
+import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
 import { useUser } from "@clerk/clerk-expo";
 import { Colors } from "../../constants/Colors";
-import { db } from "../../configs/FirebaseConfig";
-import { collection, getDocs, query } from "firebase/firestore";
-import SearchBar from "./SearchBar"; // Importar el nuevo componente
+import SearchBar from "./SearchBar";
+import SearchBusinessList from "./SearchBusinessList";
 
 export default function Header() {
   const { user } = useUser();
-  const [searchText, setSearchText] = useState(""); // Estado para el texto de búsqueda
-  const [businesses, setBusinesses] = useState([]); // Estado para la lista completa de negocios
-  const [filteredBusinesses, setFilteredBusinesses] = useState([]); // Estado para la lista filtrada de negocios
-
-  useEffect(() => {
-    fetchBusinesses(); // Llamar a fetchBusinesses cuando el componente se monta
-  }, []);
-
-  const fetchBusinesses = async () => {
-    const q = query(collection(db, "BusinessList")); // Crear una consulta a la colección "BusinessList"
-    const querySnapshot = await getDocs(q); // Obtener los documentos de la consulta
-    const businessList = [];
-    querySnapshot.forEach((doc) => {
-      businessList.push({ id: doc.id, ...doc.data() }); // Agregar cada documento a la lista de negocios
-    });
-    setBusinesses(businessList); // Establecer la lista completa de negocios
-    setFilteredBusinesses(businessList); // Inicialmente, la lista filtrada es la misma que la lista completa
-  };
-
-  const handleSearch = (text) => {
-    setSearchText(text); // Actualizar el texto de búsqueda
-    if (text) {
-      const filtered = businesses.filter((business) =>
-        business.name.toLowerCase().includes(text.toLowerCase())
-      ); // Filtrar los negocios que coinciden con el texto de búsqueda
-      setFilteredBusinesses(filtered); // Actualizar la lista filtrada de negocios
-    } else {
-      setFilteredBusinesses([]); // Establecer como lista vacía cuando no hay texto
-    }
-  };
-
-  const renderBusinessCard = ({ item }) => (
-    <View
-      style={{
-        backgroundColor: "rgba(255, 255, 255, 1)",
-        borderRadius: 10,
-        padding: 15,
-        marginBottom: 5,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        elevation: 3,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-    >
-      <View>
-        <Text style={{ fontSize: 18, fontWeight: "bold" }}>{item.name}</Text>
-        <Text style={{ color: Colors.GRAY }}>{item.address}</Text>
-      </View>
-      <View>
-        <Image
-          source={{ uri: item.imageUrl }}
-          style={{ height: 50, width: 80, borderRadius: 10 }}
-        />
-      </View>
-    </View>
-  );
+  const [searchText, setSearchText] = useState("");
 
   return (
     <View>
@@ -107,27 +47,9 @@ export default function Header() {
             </Text>
           </View>
         </View>
-        <SearchBar searchText={searchText} handleSearch={handleSearch} />
+        <SearchBar searchText={searchText} handleSearch={setSearchText} />
       </View>
-      {filteredBusinesses.length > 0 && (
-        <View
-          style={{
-            position: "absolute",
-            top: 150, // Ajusta según sea necesario
-            left: 0,
-            right: 0,
-            backgroundColor: "transparent",
-            zIndex: 1,
-          }}
-        >
-          <FlatList
-            data={filteredBusinesses}
-            keyExtractor={(item) => item.id}
-            renderItem={renderBusinessCard}
-            contentContainerStyle={{ padding: 20 }}
-          />
-        </View>
-      )}
+      <SearchBusinessList searchText={searchText} />
     </View>
   );
 }
---
+++ components/Home/SearchBusinessList.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from "react";
+import { View, FlatList, Text, Image, TouchableOpacity } from "react-native";
+import { db } from "../../configs/FirebaseConfig";
+import { collection, getDocs, query } from "firebase/firestore";
+import Fuse from "fuse.js";
+import { Colors } from "../../constants/Colors";
+import { useRouter } from "expo-router";
+import { Rating } from "react-native-ratings";
+
+// Componente principal que recibe el texto de búsqueda como prop
+const SearchBusinessList = ({ searchText }) => {
+  // Estado para almacenar la lista de negocios y la lista filtrada
+  const [businesses, setBusinesses] = useState([]);
+  const [filteredBusinesses, setFilteredBusinesses] = useState([]);
+
+  // Hook para la navegación
+  const router = useRouter();
+
+  // useEffect para cargar la lista de negocios al montar el componente
+  useEffect(() => {
+    fetchBusinesses();
+  }, []);
+
+  // useEffect para filtrar la lista de negocios cuando cambia el texto de búsqueda
+  useEffect(() => {
+    handleSearch(searchText);
+  }, [searchText]);
+
+  // Función para obtener la lista de negocios desde Firestore
+  const fetchBusinesses = async () => {
+    const q = query(collection(db, "BusinessList"));
+    const querySnapshot = await getDocs(q);
+    const businessList = [];
+    querySnapshot.forEach((doc) => {
+      businessList.push({ id: doc.id, ...doc.data() });
+    });
+    setBusinesses(businessList);
+    setFilteredBusinesses(businessList);
+  };
+
+  // Función para manejar la búsqueda utilizando Fuse.js
+  const handleSearch = (text) => {
+    if (text) {
+      const fuse = new Fuse(businesses, {
+        keys: ["name"],
+        threshold: 0.2,
+      });
+      const result = fuse.search(text);
+      const filtered = result.map(({ item }) => item);
+      setFilteredBusinesses(filtered);
+    } else {
+      setFilteredBusinesses([]);
+    }
+  };
+
+  // Función para renderizar cada tarjeta de negocio
+  const renderBusinessCard = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => router.push("/businessdetail/" + item?.id)}
+    >
+      <View
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 1)",
+          borderRadius: 10,
+          padding: 15,
+          marginBottom: 5,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 5,
+          elevation: 3,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>{item.name}</Text>
+          <Text style={{ color: Colors.GRAY }}>{item.address}</Text>
+          <Rating
+            imageSize={15}
+            ratingCount={item.Rating}
+            readonly={true}
+            style={{ alignItems: "flex-start" }}
+          />
+        </View>
+        <View>
+          <Image
+            source={{ uri: item.imageUrl }}
+            style={{
+              height: 50,
+              width: 100,
+              padding: 10,
+              borderRadius: 10,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.1,
+              shadowRadius: 5,
+              elevation: 3,
+            }}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+
+  // Renderiza la lista de negocios filtrados
+  return (
+    <View
+      style={{
+        position: "absolute",
+        top: 150,
+        left: 0,
+        right: 0,
+        backgroundColor: "transparent",
+        zIndex: 1,
+      }}
+    >
+      <FlatList
+        data={filteredBusinesses}
+        keyExtractor={(item) => item.id}
+        renderItem={renderBusinessCard}
+        contentContainerStyle={{ padding: 20 }}
+      />
+    </View>
+  );
+};
+
+export default SearchBusinessList;
